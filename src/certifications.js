40014f867ed94a7e59837a6af94bf18b2c4a12ba
acharyasagar <acharyasaagar@gmail.com>
2020-10-01 10:54:00 +0300

    add certificates data

---
+++ src/certifications.js
@@ -0,0 +1,40 @@
+export const metropolia = {
+  certificateTitle: `Bachelor's in IT Engineering`,
+  certificationAuthority: 'Helsinki Metropolia University of Applied Sciences',
+  startYear: '2016',
+  finishYear: '2020',
+  hasCertificate: false,
+  certificateLink: null,
+  imgSrc: '/images/metropolia.png',
+  imgAlt: 'Logo of Helsinki Metropolia University of Applied Sciences',
+}
+
+export const freecodecamp = {
+  certificateTitle: `Javascript Algorithms & Data Structures`,
+  certificationAuthority: 'FreeCodeCamp',
+  startYear: null,
+  finishYear: null,
+  hasCertificate: true,
+  certificateLink:
+    'https://www.freecodecamp.org/certification/acharyasagar/javascript-algorithms-and-data-structures',
+  imgSrc: '/images/freecodecamp.png',
+  imgAlt: 'Logo of freecodecamp.org',
+}
+
+export const fsopen = {
+  certificateTitle: `Full Stack Open 2020`,
+  certificationAuthority: 'Helsinki University',
+  startYear: null,
+  finishYear: null,
+  hasCertificate: true,
+  certificateLink:
+    'https://studies.cs.helsinki.fi/stats/api/certificate/fullstackopen/en/6ff8e11c1ddd0c1e4618ba3fc0de7a40',
+  imgSrc: '/images/helsinki-uni.png',
+  imgAlt: 'Logo of Helsinki University',
+}
+
+export default {
+  freecodecamp,
+  fsopen,
+  metropolia,
+}
