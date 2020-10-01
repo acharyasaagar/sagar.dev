7e2851647da27f516b5172f4906cfacb2fcd3ea8
acharyasagar <acharyasaagar@gmail.com>
2020-10-01 10:47:07 +0300

    render index page directly from the App component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-import App from '../src'
+import App from '../src/components/App'
 
-const Index = () => {
-  return <App />
-}
+const Index = () => <App />
 
 export default Index
