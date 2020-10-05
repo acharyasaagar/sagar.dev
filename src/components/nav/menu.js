8e0bee872e02948fbf0d680db56c98bdfe98a749
acharyasagar <acharyasaagar@gmail.com>
2020-10-05 10:55:38 +0300

    creates mobile menu with the drawer

---
+++ src/components/nav/menu.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: 'About',
+    href: '#about',
+  },
+  {
+    title: 'Skills',
+    href: '#skills',
+  },
+  {
+    title: 'Projects',
+    href: '#projects',
+  },
+  {
+    title: 'Education',
+    href: '#education',
+  },
+  {
+    title: 'Contact',
+    href: '#contact',
+  },
+]
