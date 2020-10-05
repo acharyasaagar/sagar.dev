caade3c766716b9e30c21fc18375a9e146e1c136
acharyasagar <acharyasaagar@gmail.com>
2020-10-05 17:15:16 +0300

    use js variable to calculate navbar height

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -4,6 +4,8 @@ import Hidden from '@material-ui/core/Hidden'
 
 import { makeStyles } from '@material-ui/core/styles'
 
+import { NAVBAR_HEIGHT } from '../../cssConstants'
+
 import DesktopMenu from './DesktopMenu'
 import MobileMenu from './MobileMenu'
 
@@ -11,7 +13,7 @@ import menuItems from './menuItems'
 
 const useStyles = makeStyles(theme => ({
   navBarContainer: {
-    height: '4rem',
+    height: NAVBAR_HEIGHT,
     padding: 0,
     margin: 0,
     display: 'flex',
