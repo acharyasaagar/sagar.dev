49ea953a3a052189829b5dd85bfce4cc8ba0eade
acharyasagar <acharyasaagar@gmail.com>
2020-10-06 11:20:49 +0300

    my skills data

---
+++ src/components/skills/mySkills.js
@@ -0,0 +1,63 @@
+import CodeSlashIcon from '../../icons/CodeSlash'
+import CurlyIcon from '../../icons/Curly'
+import DesignIcon from '../../icons/Design'
+
+export default [
+  {
+    AvatarIcon: <CodeSlashIcon />,
+    skillTitle: 'Frond-end Development',
+    skillText: `
+      I am passionate about creating user interfaces that are easy to use and 
+      bring a great experience to user. Building simple and intuitive user 
+      interfaces with great user experience is a challenging task and I embrace that challenge.
+    `,
+    toolsTitle: 'My front-end toolbox',
+    tools: [
+      'html',
+      'css',
+      'javascript',
+      'react',
+      'redux',
+      'material-ui',
+      'jest',
+    ],
+  },
+  {
+    AvatarIcon: <CurlyIcon />,
+    skillTitle: 'Back-end Development',
+    skillText: `
+      Building APIs that are well designed, well documented and well tested is a 
+      difficult task that requires a lot of planning. I enjoy designing, building and 
+      testing RESTful APIs that can be consumed by any frontend apps using HTTP.
+    `,
+    toolsTitle: 'My back-end toolbox',
+    tools: [
+      'nodejs',
+      'express',
+      'typescript',
+      'postgresql',
+      'mongodb',
+      'jest',
+      'RAML',
+      'docker',
+    ],
+  },
+  {
+    AvatarIcon: <DesignIcon />,
+    skillTitle: 'UI Design',
+    skillText: `
+      I believe I fall on the category of developer with an eye for design. 
+      I love playing with Figma to create UIs that look clean and beautiful. 
+      I especially like experimenting with combining different typographies, colors and layouts.
+    `,
+    toolsTitle: 'My design tools & resources',
+    tools: [
+      'figma',
+      'google fonts',
+      'material icons',
+      'unsplash',
+      'mycolor.space',
+      'coolors.co',
+    ],
+  },
+]
